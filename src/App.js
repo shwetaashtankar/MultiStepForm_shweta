3a53435c80241dd4d7165b0136729d4a1a594764
shwetaashtankar <shwetaashtankar1997@gmail.com>
2024-07-07 00:12:36 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import MultiStepForm from './components/MultiStepForm';
+import { FormProvider } from './context/FormContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider>
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <div className="App-body">
+          <MultiStepForm />
+        </div>
+      </div>
+    </FormProvider>
   );
 }
 
